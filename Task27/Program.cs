a0d88ffdf3fb0713de28132aa74427da2ec93eb6
unknown <teterkinaejaterina97@gmail.com>
2023-06-03 15:33:29 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n);
+
+int result = 0;
+
+while (n > 0)
+{
+    result += n % 10;
+    n /= 10;
+}
+
+Console.WriteLine(result);
