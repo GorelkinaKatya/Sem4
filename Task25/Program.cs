c56ac277f8dd6c16f0b7aa5a4bbf1b8ddf37b2e2
unknown <teterkinaejaterina97@gmail.com>
2023-06-03 15:14:42 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+for (int i = 0; i < b; i++)
+    result *= a;
+
+if (b > 0)
+    Console.WriteLine(result);
+else if (b < 0 && a != 0)
+    Console.WriteLine(1 / result);
+else if (b == 0)
+    Console.WriteLine(1);
+else
+    Console.WriteLine("Ошибка. Деление на ноль!");
