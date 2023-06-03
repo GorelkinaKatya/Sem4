f90fdfed76ec1fe2db0334977d10e3f30eb8ae94
unknown <teterkinaejaterina97@gmail.com>
2023-06-03 15:40:53 +0300

    add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int [] array = new int [8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите значение элемента массива: ");
+    int n = int.Parse(Console.ReadLine()!);
+    array[i] = n;
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
